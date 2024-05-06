37972f96122fe93b3327774a138813043bb3eac8
Rodolfo Oliveira <orodolfoso.dev@gmail.com>
2024-05-06 20:27:31 -0300

    Iniciando aula de operadores

---
+++ javascript/operadores/js/scripts.js
@@ -0,0 +1,49 @@
+// // 1 - Number
+// console.log(typeof 2);
+// console.log(typeof 5.14);
+// console.log(typeof -128);
+
+// // 2 - Operações aritiméticas
+// console.log(2 + 5);
+// console.log(5 + (4* 12));
+// console.log(10 / 2);
+// console.log(132 - 85);
+// console.log(85 * 4);
+
+// // 3 - Special Numbers
+
+// console.log(typeof Infinity);
+
+// console.log(typeof - Infinity);
+
+// console.log(typeof 12 * "asd");
+
+// console.log(typeof NaN);
+
+// // 4 - Strings
+
+// console.log("Rodolfo");
+// console.log('Olá mundo!');
+// console.log('13');
+
+// console.log(typeof "Rodolfo");
+// console.log(typeof 'Olá mundo!');
+// console.log(typeof '13');
+
+// 5 - Caracteres especiais em strings
+
+console.log("Testando a \n quebra de linha");
+
+console.log("Espaçamento \t tab");
+
+// 6 - Concatenação
+
+console.log("Maria," + " como você" + " está?");
+
+console.log('Testando' + ' com' + ' crase!');
+
+
+ 
+
+
+
