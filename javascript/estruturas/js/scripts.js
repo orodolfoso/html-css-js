be49503a2bb00dc12c6815f850178e831c9046dd
Rodolfo Oliveira <orodolfoso.dev@gmail.com>
2024-05-07 11:22:46 -0300

    Iniciando aula de estruturas

---
+++ javascript/estruturas/js/scripts.js
@@ -0,0 +1,14 @@
+//  1 - Variáveis
+let nome = "Rodolfo";
+console.log(nome);
+
+nome = "Rodolfo Oliveira";
+console.log(nome);
+
+const idade = 39;
+console.log(idade);
+
+// idade = 38;
+
+console.log(typeof nome);
+console.log(typeof idade);
--- javascript/operadores/js/scripts.js
+++ javascript/operadores/js/scripts.js
@@ -1,34 +1,34 @@
 // // 1 - Number
-// console.log(typeof 2);
-// console.log(typeof 5.14);
-// console.log(typeof -128);
+console.log(typeof 2);
+console.log(typeof 5.14);
+console.log(typeof -128);
 
-// // 2 - Operações aritiméticas
-// console.log(2 + 5);
-// console.log(5 + (4* 12));
-// console.log(10 / 2);
-// console.log(132 - 85);
-// console.log(85 * 4);
+// 2 - Operações aritiméticas
+console.log(2 + 5);
+console.log(5 + (4* 12));
+console.log(10 / 2);
+console.log(132 - 85);
+console.log(85 * 4);
 
 // // 3 - Special Numbers
 
-// console.log(typeof Infinity);
+console.log(typeof Infinity);
 
-// console.log(typeof - Infinity);
+console.log(typeof - Infinity);
 
-// console.log(typeof 12 * "asd");
+console.log(typeof 12 * "asd");
 
-// console.log(typeof NaN);
+console.log(typeof NaN);
 
 // // 4 - Strings
 
-// console.log("Rodolfo");
-// console.log('Olá mundo!');
-// console.log('13');
+console.log("Rodolfo");
+console.log('Olá mundo!');
+console.log('13');
 
-// console.log(typeof "Rodolfo");
-// console.log(typeof 'Olá mundo!');
-// console.log(typeof '13');
+console.log(typeof "Rodolfo");
+console.log(typeof 'Olá mundo!');
+console.log(typeof '13');
 
 // 5 - Caracteres especiais em strings
 
@@ -36,11 +36,83 @@ console.log("Testando a \n quebra de linha");
 
 console.log("Espaçamento \t tab");
 
-// 6 - Concatenação
+// // 6 - Concatenação
 
 console.log("Maria," + " como você" + " está?");
 
 console.log('Testando' + ' com' + ' crase!');
+
+// 7 - Interpolação - Templates Strings
+
+console.log(`A soma de 2 + 2 é:  ${2 + 2}`);
+console.log(`Podemos executar qualquer coisa aqui ${console.log("teste")}`)
+
+ //  8 - Boolean
+
+console.log(true);
+console.log(5 > 10);
+console.log(10 < 2);
+console.log(8 > 4);
+console.log(typeof false);
+
+// //  9 - Comparações
+
+console.log( 8 == 4);
+console.log(10 >= 9);
+console.log(10 != 8);
+console.log(10 === 50);
+console.log(50 <= 5);
+ 
+// // 10 - Idêntico
+
+console.log(9 == "9");
+console.log(9 === "9");
+console.log(9 != "9");
+console.log(9 !== "9");
+
+// 11 - Operadores lógicos
+
+console.log(true && true);
+
+console.log(true && false);
+
+console.log(5 > 2 && 2 < 10);
+
+console.log(5 > 2 && "Rodolfo" === 1);
+
+console.log(5 > 2 || "Rodolfo" === 1);
+
+console.log(5 < 2 || 5 > 100);
+
+console.log(!true);
+
+console.log(!false);
+
+console.log(!5 < 2);
+
+// // 12 - Empty Values
+
+console.log(typeof null, typeof undefined);
+
+console.log(null == undefined);
+console.log(null === undefined);
+
+console.log(null == false);
+console.log(undefined == false);
+
+
+// 13 - Mudança de tipos
+
+console.log(5 * null);
+console.log("teste" * "opa");
+console.log("10" + 1);
+console.log("10" - 1); 
+
+
+
+
+
+
 
 
  
