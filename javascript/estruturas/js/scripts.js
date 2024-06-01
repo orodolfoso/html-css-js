8dc0a9560d273a39a7e757b203df06f93f6c4d11
Rodolfo Oliveira <orodolfoso.dev@gmail.com>
2024-06-01 15:41:33 -0300

    Iniciando aula de funções

--- javascript/estruturas/js/scripts.js
+++ javascript/estruturas/js/scripts.js
@@ -12,3 +12,54 @@ console.log(idade);
 
 console.log(typeof nome);
 console.log(typeof idade);
+
+// 2 - Mais sobre variáveis
+  
+// let 2teste = "inválido"
+// let @teste = "inválido"
+
+let a = 10,
+b = 20,
+c = 30;
+
+console.log(a, b, c);
+
+const nomecompleto = "Rodolfo Oliveira";
+
+const nomeCompleto = "Lucas de Oliveira";
+
+console.log(nomecompleto);
+console.log(nomeCompleto);
+
+let _teste = "ok";
+
+let $teste = "ok";
+
+console.log(_teste, $teste);
+
+// 3 - Nomes reservados
+
+console.log("ok");
+
+// 4 - Função de prompt
+
+// const age = prompt ("Digite a sua idade");
+// console.log(`Você tem ${age} anos.`);
+
+// Função de alerta
+
+// alert("Qual é o seu nome?");
+
+// const z = 10
+
+// alert(`O número é ${z}`);
+
+// 5 - Funções do JS: Math.x
+
+// console.log(Math.max(5, 2, 1, 10)); apresenta o maior valor
+
+// console.log(Math.floor(5.14)); arredonda o valor
+
+// console.log(Math.ceil(5.14)); sobe o valor
+
+
