909bcc5d7b4aaf053ced2341f831246359d74cee
Rodolfo Oliveira <orodolfoso.dev@gmail.com>
2024-06-17 17:23:18 -0300

    Conhecendo melhor os objetos

--- javascript/arrays_e_objs/js/scripts.js
+++ javascript/arrays_e_objs/js/scripts.js
@@ -61,36 +61,79 @@
 
 // 5 - Criando e deletando propriedades
 
-const car = {
-    engine: 2.0,
-    brand: "VW",
-    model: "Tiguan",
-    km: 20000,
-};
+// const car = {
+//     engine: 2.0,
+//     brand: "VW",
+//     model: "Tiguan",
+//     km: 20000,
+// };
 
-console.log(car);
+// console.log(car);
 
-car.doors = 4;
+// car.doors = 4;
 
-console.log(car);
+// console.log(car);
 
-delete car.brand;
+// delete car.brand;
 
-console.log(car);
+// console.log(car);
 
-// 6 - Mais sobre objetos
+// // 6 - Mais sobre objetos
 
-const obj = {
-    a: "teste",
-    b:  true,
-};
+// const obj = {
+//     a: "teste",
+//     b:  true,
+// };
 
-console.log(obj instanceof object);
+// console.log(obj instanceof Object);
 
-const obj2 = {
-    c: [],
+// const obj2 = {
+//     c: [],
+// }
+
+// Object.assign(obj2, obj);
+
+// console.log(obj2);
+
+// // 7 - Conhecendo melhor os objetos
+
+// console.log(Object.keys(obj));
+// console.log(Object.keys(obj2));
+// console.log(Object.keys(car));
+
+// console.log(Object.entries(car));
+
+// // 8 - Mutação (Mutability)
+
+// const a = {
+//     name: "Rodolfo",
+// };
+
+// const b = a;
+
+// console.log(a);
+// console.log(b);
+
+// console.log(a === b);
+
+// a.age = 39;
+
+// console.log(a);
+// console.log(b);
+
+// delete b.age
+
+// console.log(a);
+// console.log(b);
+
+// 9 - Loop em array
+
+const users = ["Rodolfo", "Lucas", "Bianca", "Renata"]
+
+
+for(let i = 0; i < users.length; i++) {
+    console.log(`Listando o usuário: ${users[i]}`);
 }
 
-Object.assign(obj2, obj);
 
-console.log(obj2);
+
