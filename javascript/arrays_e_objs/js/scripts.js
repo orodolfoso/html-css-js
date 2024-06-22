b9fa17ea5138d153220027e641ae31fe0eb547f9
Rodolfo Oliveira <orodolfoso.dev@gmail.com>
2024-06-22 20:06:09 -0300

    Aula sobre Includes, concluído

--- javascript/arrays_e_objs/js/scripts.js
+++ javascript/arrays_e_objs/js/scripts.js
@@ -213,5 +213,34 @@ const subArray4 = testeSlice.slice(2);
 
 console.log(subArray4);
 
+// 14 - forEach
+
+const nums = [1,2,3,4,5];
+
+nums.forEach((numero) => {
+    console.log(`O número é ${numero}`);
+})
+
+const posts = [
+    {title: "Primeiro post", category: "PHP"},
+    {title: "Segundo post", category: "Javascript"},
+    {title: "Terceiro post", category: "Python"},
+];
+
+posts.forEach((post) =>{
+    console.log(`Exibindo post: ${post.title}, da categoria: ${post.category}`);
+})
+
+// 15 - Includes
+
+const brands = ["BMW", "VW", "FIAT"];
+
+console.log(brands.includes("FIAT"));
+
+console.log(brands.includes("KIA"));
+
+if(brands.includes("BMW")) {
+    console.log("Há carros da marca BMW!");
+}
 
 
