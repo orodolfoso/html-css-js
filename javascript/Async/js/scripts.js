7b7555040178f17af847cfffa6664457affaf6ea
Rodolfo Oliveira <orodolfoso.dev@gmail.com>
2024-07-01 17:09:01 -0300

    Iniciando a seção sobre Programação Assincrona

---
+++ javascript/Async/js/scripts.js
@@ -0,0 +1,38 @@
+// 1 - setTimeout
+
+console.log("Ainda não executou");
+
+setTimeout(function() {
+    console.log("Requisição assincrona")
+
+}, 2000);
+
+
+console.log("Ainda não executou 2");
+
+// 2 - setInterval
+
+console.log("Ainda não começou");
+
+// setInterval(function() {
+//     console.log("Intervalo assincorono");
+// }, 3000);
+
+console.log("Ainda não começou 2");
+
+// 3 - Promises
+
+const promessa = Promise.resolve(5 + 5)
+
+console.log("Algum código")
+
+promessa.then((value) => {
+    console.log(`A soma é ${value}`);
+    return value;
+})
+
+.then((value) => value - 1)
+.then((value) => console.log(`Agora é ${value}`));
+
+console.log("Outro código")
+
--- javascript/debug/js/scripts.js
+++ javascript/debug/js/scripts.js
@@ -31,3 +31,72 @@ if (a > 5) {
 }
 
 console.log(a);
+
+// 3 - Tratamento de dado por função
+
+function checkNumber(n) {
+    const result = Number(n)
+
+    if(Number.isNaN(result)) {
+        console.log("Valor incorreto!");
+        return result;
+    }
+    
+    console.log("Valor correto!")
+    return result;
+
+}
+
+checkNumber(5);
+checkNumber("10");
+checkNumber({});
+checkNumber("teste");
+
+// 4 - Exceptions
+
+let x = 10
+
+if(x != 11) {
+    // throw new Error("O valor de x não pode ser diferente de 11!");
+
+}
+
+// 5 - Try Catch
+
+try {
+    const soma = x + y;
+
+} catch(error) {
+    console.log(`Erro no programa: ${error}`);
+}
+ 
+
+// 6  - Finally
+
+try {
+    const value = checkNumber("1")
+
+        if (!value) {
+            throw Error("Valores inválidos")
+        }
+} catch (error) {
+    console.log(`Opa, aconteceu um problema ${error}`);
+} finally {
+    console.log("O código foi executado!");
+    
+}
+
+// 7 - Assertions
+
+function checkArray(arr) {
+    if (arr.length === 0) {
+        throw new Error ("O array precisa ter elementos");
+    } else{
+        console.log(`O array tem ${arr.length} elementos`);
+    }
+}
+
+// checkArray([]);
+
+
+checkArray([1, 2, 3]);
