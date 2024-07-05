66ba5f9929f2b26e8a3dd220b04137a0e25b7a61
Rodolfo Oliveira <orodolfoso.dev@gmail.com>
2024-07-05 11:06:03 -0300

    Trabalhando com atributos

--- javascript/dom/js/scripts.js
+++ javascript/dom/js/scripts.js
@@ -36,5 +36,63 @@ const mainContainer = document.querySelectorAll("#main-container");
 
 console.log(mainContainer);
 
+// 6 - insertBefore
+
+const p = document.createElement("p");
+
+const header = title.parentElement;
+
+header.insertBefore(p, title);
+
+// 7 - appendChild
+
+const navLink = document.querySelectorAll("nav ul");
+
+const li = document.createElement("li");
+
+navLink.appendChild(li);
+
+// 8 - replaceChild
+
+const h2 = document.createElement("h2");
+
+h2.textContent = "Meu novo título!";
+
+header.replaceChild(h2, title);
+
+// 9 - createTExtNode
+
+const myText = document.createTextNode("Agora vamos colocar mais um título");
+
+console.log(myText);
+
+const h3 = document.createElement("h3");
+
+h3.appendChild(myText);
+
+console.log(h3);
+
+mainContainer[0].appendChild(h3);
+
+// 10 -  Trabalhando com atributos
+
+const firstLink = navLink.querySelector("a");
+
+console.log(firstLink);
+
+firstLink.setAttibute("href", "https://www.google.com");
+
+console.log(firstLink.getAttribute("href"));
+
+firstLink.setAttibute("target", "_blank");
+
+
+
+
+
+
+
+
+
 
 
