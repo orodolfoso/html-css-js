6a9dcc8d684f156b4695981db83e76616362fc18
Rodolfo Oliveira <orodolfoso.dev@gmail.com>
2024-07-07 15:20:58 -0300

    Aprendendo sobre Propagação

--- javascript/eventos/js/scripts.js
+++ javascript/eventos/js/scripts.js
@@ -6,3 +6,47 @@ btn.addEventListener("click", function() {
     console.log("Clicou aqui!");
 
 });
+
+// 2 - Removendo eventos
+
+const secondBtn = document.querySelector("#btn");
+
+
+function imprimirMensagem() {
+    console.log("Teste");
+}
+
+secondBtn.addEventListener("click", imprimirMensagem);
+
+const thirdBtn = document.querySelector("#other-btn");
+
+thirdBtn.addEventListener("click", () => {
+    console.log("Evento removido");
+    secondBtn.removeEventListener("click", imprimirMensagem);
+});
+
+// 3 - Argumento do evento
+
+const myTitle = document.querySelector ("#my-title");
+
+myTitle.addEventListener("click", (event) => {
+    console.log(event);
+    console.log(event.offsetX);
+    console.log(event.pointerType);
+    console.log(event.target);
+});
+
+// 4 - Propagando eventos
+
+const containerBtn = document.querySelector("#btn-container");
+
+const btnInsideContainer = document.querySelector("#div-btn");
+
+containerBtn.addEventListener("click", () => {
+    console.log("Evento 1");
+});
+
+btnInsideContainer.addEventListener("click", (e) => {
+    e.stopPropagation();
+    console.log("Evento 2");
+});
